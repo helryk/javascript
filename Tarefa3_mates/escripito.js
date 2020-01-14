b93dd6864b70db8be2be8be9f807aad3d7d7a48d
helryk <56599407+helryk@users.noreply.github.com>
2020-01-14 12:52:14 +0100

    Add files via upload

---
+++ Tarefa3_mates/escripito.js
@@ -0,0 +1,31 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: 
+*/
+
+
+var coche = 5.7766;
+var mesa = 4.8767;
+var silla = 6.8767:
+var pi = Math.PI;
+
+var caso1 = Math.sqrt(coche);
+alert(caso1);
+/*Raíz Cuadrada*/
+
+var caso2 = Math.pow(mesa,2);
+alert(caso2);
+/*Cuadrado*/
+
+var caso3 = Math.round(pi);
+alert(caso3);
+/*Número PI*/
+
+var caso4 = Math.min(coche, mesa, silla);
+alert(caso4);
+/*Función que responde con la menor de las variables*/
+
+var caso5 = Math.random(0,3);
+alert(caso5);
+/*Raíz Cuadrada*/
