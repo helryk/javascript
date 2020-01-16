eca4168bc81384fb5ba82e2207508520df481298
helryk <56599407+helryk@users.noreply.github.com>
2020-01-16 10:38:23 +0100

    Add files via upload

---
+++ Tarefa5_ducias2ovos/escripito.js
@@ -0,0 +1,15 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: 
+*/
+
+
+var ducias;
+var numducias;
+ducias = prompt("Cantas ducias de ovos leva unha tortilla");
+numducias = parseInt(ducias);
+alert(ducias);
+var ovos;
+ovos = 12* numducias;
+alert(ovos)
